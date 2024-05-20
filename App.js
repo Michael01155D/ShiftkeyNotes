61cbd1a27c2b1b83195b4215075199c27f94ce8a
Michael Mroz <michaelmroz01111@gmail.com>
2024-05-20 15:25:09 -0300

    add style for add button, refactor App to get notes from db

--- App.js
+++ App.js
@@ -1,36 +1,48 @@
-import { SafeAreaView, Text, View } from 'react-native';
+import { Pressable, SafeAreaView, Text, View } from 'react-native';
 import tw, { useDeviceContext } from 'twrnc';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import 'react-native-reanimated'; 
-import Note from './components/Note';
 import { useEffect, useState } from 'react';
-import { dbApi } from './db';
+import { useAddNoteMutation, useFetchNotesQuery, useDeleteNoteMutation } from './db';
+import Note from "./components/Note";
+import { TouchableOpacity } from 'react-native-web';
+
+const HomePage = () => {
+  const [notes, setNotes] = useState([]);
+  const { data } = useFetchNotesQuery();
+  const [ addNote ] = useAddNoteMutation();
+  const [ deleteNote ] = useDeleteNoteMutation();
+
+  useEffect(() => { setNotes(data) }, []);
+
+  return(
+    <>
+      { notes ? 
+        notes.map((n, i) => <Note key={i} content={n}/>)
+        : <></>
+      }
+    </>
+  )
+}
 
 function App() {
   useDeviceContext(tw);
-  const [notes, setNotes] = useState([]);
-  const getNotes = async () => {
-    const notesReady = await dbApi.useFetchNotesQuery();
-    return notesReady;
-  }
-  const twText = `text-white text-center mt-5`;
-  useEffect(() => {
-    setNotes(getNotes);
-  }, [])
-  console.log("notes is:" , notes)
+  const twText = "w-screen mt-16 text-center text-xl text-white font-bold";
+ 
   return (
     <Provider store={store}>
       <SafeAreaView style={tw`bg-black`}>
-        <Text style={tw`w-screen mt-16 text-center text-xl text-white font-bold`}>
+        <Text style={tw`${twText}`}>
           Notes
         </Text>
-        <View>
-          <Text style={tw`${twText}`}>Search bar goes here</Text>
-        </View>
-        <View>
-          <Text style={tw`${twText}`}>Notes go here</Text>
-        </View>
+        <Text style={tw`${twText}`}>
+          Search bar goes here
+        </Text>
+         <HomePage />
+         <Pressable style={tw` absolute bottom-15 right-10 w-12 h-12 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded-full`}onPress={() => {}}>
+            <Text style={tw`m-auto text-white text-xl font-extrabold`}>+</Text>
+          </Pressable> 
       </SafeAreaView>
     </Provider>
   )
--- components/Note.jsx
+++ components/Note.jsx
@@ -1,11 +1,13 @@
 import { Text } from 'react-native';
 import tw from 'twrnc';
 
-const Note = ({ title, content}) => {
+const Note = ({ title, content }) => {
+    const twTitle = "w-screen mt-10 text-center text-2xl text-white font-bold";
+    const twText = "w-screen mt-4 text-center text-xl text-white font-bold";
     return (
         <>
-        <Text style={tw`text-white-500`}>{title}</Text>
-        <Text>{content}</Text>
+        <Text style={tw`${twTitle}`}>{title}</Text>
+        <Text style={tw`${twText}`}>{content}</Text>
         </>
     )
 }
