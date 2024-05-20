d87089bfae995fde8e4cba85c439659fd5cc1617
Michael Mroz <michaelmroz01111@gmail.com>
2024-05-20 15:42:56 -0300

    improve button styling

--- App.js
+++ App.js
@@ -6,8 +6,8 @@ import 'react-native-reanimated';
 import { useEffect, useState } from 'react';
 import { useAddNoteMutation, useFetchNotesQuery, useDeleteNoteMutation } from './db';
 import Note from "./components/Note";
-import { TouchableOpacity } from 'react-native-web';
 
+//todo: move components into different files
 const HomePage = () => {
   const [notes, setNotes] = useState([]);
   const { data } = useFetchNotesQuery();
@@ -40,7 +40,7 @@ function App() {
           Search bar goes here
         </Text>
          <HomePage />
-         <Pressable style={tw` absolute bottom-15 right-10 w-12 h-12 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded-full`}onPress={() => {}}>
+         <Pressable style={tw` absolute bottom-15 right-10 w-12 h-12 bg-blue-500 hover:bg-blue-700 text-white font-bold border border-blue-700 rounded-full`}onPress={() => {}}>
             <Text style={tw`m-auto text-white text-xl font-extrabold`}>+</Text>
           </Pressable> 
       </SafeAreaView>
