e4e9e6c03a05f5a8c27b76648356afc4b841b767
Michael Mroz <michaelmroz01111@gmail.com>
2024-05-23 22:48:54 -0300

    clicking button now adds note to db and changes screen. next todo: make note update title&content

--- App.js
+++ App.js
@@ -4,48 +4,48 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import 'react-native-reanimated'; 
 import { useEffect, useState } from 'react';
-import { useAddNoteMutation, useFetchNotesQuery, useDeleteNoteMutation } from './db';
+import { useAddNoteMutation, useFetchNotesQuery, useDeleteNoteMutation, useSearchNotesQuery } from './db';
 import Note from "./components/Note";
 import MasonryList from '@react-native-seoul/masonry-list';
 import { TextInput } from 'react-native-web';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-//next todo: implement stack.screen
+const Stack = createNativeStackNavigator();
 
 //todo: move components into different files
 const HomePage = ( {navigation} ) => {
-  const [notes, setNotes] = useState();
+  const [notes, setNotes] = useState([]);
   //aliasing to prevent name conflict
-  const { data: notesData } = useFetchNotesQuery();
+  const { data: notesData } = useSearchNotesQuery("");
   const [addNote, { data: newNoteData, error: addNoteError}] = useAddNoteMutation();
   
   useEffect(() => {
-    if (notesData) { 
-      setNotes(notesData);
-    } else{   //for debugging
-      setNotes([{title: "hello", content: "world"}]);
-    } 
-    console.log("Notes are", notes);
-    }, []);
+    if (newNoteData != undefined) {
+      navigation.navigate("NoteEditor", {data: newNoteData});
+    }
+  }, [newNoteData])
   
+
   return(
-    notes ?
-    <View style={tw`w-full h-screen`}>
-      <SearchBar/>
-      <Notes notes={notes}/>
-      <Pressable style={tw` absolute bottom-15 right-10 w-12 h-12 bg-blue-500 hover:bg-blue-700 text-white font-bold border border-blue-700
+    notesData ?
+    <SafeAreaView style={tw`bg-black h-full w-full`}>
+        <SearchBar/>
+        <Notes notes={notesData}/>
+        <Pressable style={tw` absolute bottom-15 right-10 w-12 h-12 bg-blue-500 hover:bg-blue-700 text-white font-bold border border-blue-700
         rounded-full`}
         onPress={() => addNote({title: "", content: ""})}
         >
           <Text style={tw`m-auto text-white text-xl font-extrabold`}>+</Text>
-      </Pressable> 
-    </View>
+        </Pressable> 
+    </SafeAreaView>
     :
     <></>
   )
 }
 
 const Notes = ({ notes }) => {
-
+  console.log("In notes component, notes is:", notes);
   return(
     <>
   <MasonryList 
@@ -65,7 +65,7 @@ const SearchBar = ({query, setQuery}) => {
   return(
     <>
     <TextInput 
-      style={tw`w-99% h-8 ml-1 mt-2 mb-2 text-white bg-slate-800 rounded-lg px-2`} 
+      style={tw`w-98% h-8 mt-2 mb-2 text-white bg-slate-800 rounded-lg px-2`} 
       placeholder="Search"
       value={query}
       onChangeText={(newQuery) => setQuery(newQuery)}
@@ -75,19 +75,49 @@ const SearchBar = ({query, setQuery}) => {
   )
 }
 
+const NoteEditor = ({route}) => {
+  const [noteTitle, setNoteTitle] = useState("");
+  const [noteContent, setNoteContent] = useState("");
+  const { title, content, id } = route.params.data;
+  const [deleteNote] = useDeleteNoteMutation();
+  useEffect(() => {
+    const updateNote = () => {
+      title = noteTitle;
+      content = noteContent;
+    }
+    //if new note is blank, delete it, else update it on unmount
+    return title === "" && content === "" ? deleteNote(id) : updateNote();
+  }, [])
+  return(
+    <SafeAreaView>
+      <TextInput placeholder="Title" onChangeText={(newText) => setNoteTitle(newText)}></TextInput>
+      <TextInput placeholder="New note" onChangeText={(newText) => setNoteContent(newText)}></TextInput>
+    </SafeAreaView>
+  )
+}
+
 function App() {
   useDeviceContext(tw);
   return (
     <Provider store={store}>
-      <Stack.Navigator initialRouteName="HomeScreen">
-      <SafeAreaView style={tw`bg-black`}>
-        <Text style={tw`w-screen mt-6 text-center text-xl text-white font-bold`}>
-          Notes
-        </Text>
-        <Stack.Screen
-        />
-      </SafeAreaView>
-      </Stack.Navigator>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="HomePage">
+          <Stack.Screen name="HomePage" component={HomePage} options={{
+              headerTitle: "Notes",
+              headerStyle: tw`bg-black`,
+              headerTintColor: "#fff",
+              headerShadowVisible: false,
+              headerTitleAlign: "center"
+            }}/>
+          <Stack.Screen name="NoteEditor" component={NoteEditor} options={{
+            headerTitle: "Notes",
+            headerStyle: tw`bg-black`,
+            headerTintColor: "#fff",
+            headerShadowVisible: false,
+            headerRight: () => (<Pressable>todo: make this look like a garbage can and delete note</Pressable>)
+          }}/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   )
 }
